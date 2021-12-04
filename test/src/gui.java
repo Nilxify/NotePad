04b04d2258aac598799638ea7ec361e316a62fc4
Jose <36005764+rhet34@users.noreply.github.com>
2021-12-04 13:12:07 -0500

    Add files via upload

--- test/src/gui.java
+++ test/src/gui.java
@@ -12,25 +12,27 @@ public class gui {
         f.setSize(800,800);
 
         //textArea.setBounds(150,50, 600, 680);
-
+        // This is for top title on gui
         JTextField title1; // uneditable
         title1 = new JTextField("Welcome to Notes V1");
         JPanel panelTop = new JPanel();
         panelTop.add(title1);
         f.add(panelTop, BorderLayout.NORTH);
-        //title1.setBounds(350,5, 125,30);
+        title1.setBounds(350,5, 125,30);
         title1.setEditable(false); // makes title1 uneditable
-        f.add(panelTop);
 
+        // areas for writing notes
         JTextArea textArea = new JTextArea(5, 10);
         JPanel panelCenter = new JPanel();
-        textArea.setBounds(150,50, 300, 300);
+        textArea.setPreferredSize(new Dimension(600,740));
+        //textArea.setBounds(150,100, 300, 300);
         panelCenter.add(textArea);
         f.add(panelCenter, BorderLayout.CENTER);
         textArea.setLineWrap(true);
 
 
-        JButton buttonIdk = new JButton("hello");
+        //one of many buttons, not sure what for at the moment
+        JButton buttonIdk = new JButton("IDK Yet");
         JPanel panelWest = new JPanel();
         buttonIdk.setPreferredSize(new Dimension(100,30));
         panelWest.add(buttonIdk);
